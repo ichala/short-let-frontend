8a8cdfb0b50a5df1a965b10331f59ac363b749d8
Lekan <jimoridwanlekan@gmail.com>
2022-09-18 09:49:08 +0100

    Update app routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { isLoggedIn } from './redux/slices/auth';
-import SideBar from './components/SideBar';
 import Login from './Pages/Auth/Login';
 import Register from './Pages/Auth/Register';
 import Home from './Pages/Home/Home';
 import './App.css';
+import Layout from './Layouts/Layout';
 
 function App() {
   const dispatch = useDispatch();
@@ -16,13 +16,13 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <SideBar>
+        <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
-        </SideBar>
+        </Layout>
       </BrowserRouter>
     </>
   );
