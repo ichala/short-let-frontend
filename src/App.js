3a853f2f3d64532f1e055f324990d9a64c83c229
Ali Jendoubi <alijandhd@gmail.com>
2022-09-18 18:50:28 +0100

    Update Index & App .js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useRoutes } from 'react-router-dom';
 import { isLoggedIn } from './redux/slices/auth';
 import './App.css';
 import Layout from './Layouts/Layout';
-import routes from './config/routes';
+import routesConfig from './config/routes';
 
 function App() {
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
+  const routes = useRoutes(routesConfig);
   useEffect(() => {
     dispatch(isLoggedIn());
   }, []);
   return (
     <>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            {routes.map((route, i) => (
-              <>
-                {route.path === '/' ? (<Route key={i} exact path={route.path} element={route.component} />) : ((user && route.protected && <Route key={i} path={route.path} element={route.component} />) || (!user && !route.protected && <Route key={i} path={route.path} element={route.component} />))}
-              </>
-            ))}
 
-          </Routes>
-        </Layout>
-      </BrowserRouter>
+      <Layout>
+        {routes}
+      </Layout>
+
     </>
   );
 }
