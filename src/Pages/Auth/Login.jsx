5646ce75cc1b98e0fc6e1db10e69bc806d83b862
Ali Jendoubi <alijandhd@gmail.com>
2022-09-19 10:42:30 +0100

    Add Login & Logout features

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import UserLogin from '../../Api/ApiCalls';
 
 function Login() {
+  const [FormData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = FormData;
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
   if (user) {
@@ -10,7 +24,25 @@ function Login() {
   }
   return (
     <>
-      <button className="btn btn-success" type="button" onClick={() => UserLogin({ email: 'admin@admin.com', password: 'admin123' }, dispatch)}>LOGIN</button>
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        UserLogin(FormData, dispatch);
+      }}
+      >
+        <div className="mb-3">
+          <div id="emailHelp" className="form-text">Email</div>
+          <input type="email" required onChange={onChange} name="email" value={email} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+
+        </div>
+        <div className="mb-3">
+          <div id="emailHelp" className="form-text">Password</div>
+          <input onChange={onChange} required name="password" value={password} type="password" className="form-control" id="exampleInputPassword1" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+
     </>
   );
 }
