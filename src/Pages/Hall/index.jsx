36c9338a368a9a1ffa26d40baf4003c773d1bc5a
Aimal Amiri <meaimal@hotmail.com>
2022-09-19 21:28:46 +0430

    Design halls table

--- src/Pages/Hall/index.jsx
+++ src/Pages/Hall/index.jsx
@@ -1,6 +1,34 @@
+import { FaPlus } from 'react-icons/fa';
+
 const Hall = () => {
-  const title = 'Halls';
-  return <h1>{title}</h1>;
+  const m = 'No Hall Found';
+  return (
+    <div className="p-5">
+      <div>
+        <a href="/" className="btn btn-success align-self-center mb-3">
+          <FaPlus />
+          New Hall
+        </a>
+      </div>
+      <table className="table table-condensed">
+        <thead className="table-header">
+          <tr>
+            <th>Name</th>
+            <th>Capacity</th>
+            <th>Cost</th>
+            <th>Description</th>
+            <th>Created By</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="text-center">
+            <td colSpan="6">{m}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Hall;
