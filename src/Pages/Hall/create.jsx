49039f940566fe3868b5c9d7c7cb2b6cc8f2a621
Aimal Amiri <meaimal@hotmail.com>
2022-09-21 13:59:08 +0430

    Add swal message to create hall

--- src/Pages/Hall/create.jsx
+++ src/Pages/Hall/create.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
-/* eslint-disable jsx-a11y/label-has-associated-control */
+import Swal from 'sweetalert2';
 import { useState } from 'react';
 import { FaPlus } from 'react-icons/fa';
 import { addHall } from '../../Api/halls/api';
@@ -23,6 +22,12 @@ const Create = ({ setChanged }) => {
     addHall(form).then(() => {
       setClose(true);
       setChanged(true); // Send a signla to the parent class.
+      Swal.fire({
+        title: 'Created',
+        text: 'Hall has been successfully created!',
+        icon: 'success',
+        confirmButtonText: 'Okay',
+      });
     });
     setForm(data);
     setClose(false); // Allow the hall to have the closing ability next time.
@@ -40,81 +45,81 @@ const Create = ({ setChanged }) => {
     >
       <form onSubmit={submit}>
         <div className="form-group mb-2">
-          <label htmlFor="name" className="fomr-label">
+          <label htmlFor="name" className="fomr-label w-100">
             Name:
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="form-control"
+              placeholder="Hall name"
+              value={name}
+              onChange={handleChange}
+              required
+            />
           </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="form-control"
-            placeholder="Hall name"
-            value={name}
-            onChange={handleChange}
-            required
-          />
         </div>
         <div className="form-group mb-2">
-          <label htmlFor="capacity" className="fomr-label">
+          <label htmlFor="capacity" className="fomr-label w-100">
             Capacity:
+            <input
+              type="number"
+              name="capacity"
+              id="capacity"
+              className="form-control"
+              placeholder="Hall capacity"
+              onChange={handleChange}
+              value={capacity}
+              required
+              min={10}
+              max={10000}
+            />
           </label>
-          <input
-            type="number"
-            name="capacity"
-            id="capacity"
-            className="form-control"
-            placeholder="Hall capacity"
-            onChange={handleChange}
-            value={capacity}
-            required
-            min={10}
-            max={10000}
-          />
         </div>
         <div className="form-group mb-2">
-          <label htmlFor="cost" className="fomr-label">
+          <label htmlFor="cost" className="fomr-label w-100">
             Cost:
+            <input
+              type="number"
+              name="cost"
+              id="cost"
+              className="form-control"
+              placeholder="Hall cost"
+              onChange={handleChange}
+              value={cost}
+              required
+            />
           </label>
-          <input
-            type="number"
-            name="cost"
-            id="cost"
-            className="form-control"
-            placeholder="Hall cost"
-            onChange={handleChange}
-            value={cost}
-            required
-          />
         </div>
         <div className="form-group mb-2">
-          <label htmlFor="image" className="fomr-label">
+          <label htmlFor="image" className="fomr-label w-100">
             Image:
+            <input
+              type="text"
+              name="image"
+              id="image"
+              className="form-control"
+              placeholder="Hall image link"
+              onChange={handleChange}
+              value={image}
+              required
+            />
           </label>
-          <input
-            type="text"
-            name="image"
-            id="image"
-            className="form-control"
-            placeholder="Hall image link"
-            onChange={handleChange}
-            value={image}
-            required
-          />
         </div>
         <div className="form-group mb-2">
-          <label htmlFor="description" className="fomr-label">
+          <label htmlFor="description" className="fomr-label w-100">
             Description:
+            <textarea
+              name="description"
+              id="description"
+              className="form-control"
+              placeholder="Hall description"
+              onChange={handleChange}
+              value={description}
+              required
+              minLength={10}
+            />
           </label>
-          <textarea
-            name="description"
-            id="description"
-            className="form-control"
-            placeholder="Hall description"
-            onChange={handleChange}
-            value={description}
-            required
-            minLength={10}
-          />
         </div>
         <button type="submit" className="btn btn-success">
           Submit
