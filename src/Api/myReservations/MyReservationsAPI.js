0a865a7f1d165459baa3cfd48321096385be5f75
EidHachem <eidhachem1@gmail.com>
2022-09-21 13:55:30 +0300

    Add api call for available reservations

---
+++ src/Api/myReservations/MyReservationsAPI.js
@@ -0,0 +1,18 @@
+import axios from '../../config/axios';
+
+export default async function GetReservationStats(error, stats) {
+  await axios
+    .get('/user/reservations')
+    .then((res) => {
+      if (res.status === 200) {
+        stats(res.data);
+      }
+    })
+    .catch((e) => {
+      if (e.toJSON().message === 'Network Error') {
+        error('No Internet Or Server is not running');
+      } else {
+        error(e.response.data.error);
+      }
+    });
+}
