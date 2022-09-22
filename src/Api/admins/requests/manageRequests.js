7786f6909623ea9969e1e09996de444f3e19bf6b
EidHachem <eidhachem1@gmail.com>
2022-09-22 20:38:32 +0300

    Add request functions to show and update requests

---
+++ src/Api/admins/requests/manageRequests.js
@@ -0,0 +1,22 @@
+import axios from '../../../config/axios';
+
+export default async function fetchRequests(error, stats) {
+  await axios
+    .get('admin/requests/pending')
+    .then((res) => {
+      if (res.status === 200) {
+        stats(res.data);
+      }
+    })
+    .catch((e) => {
+      if (e.toJSON().message === 'Network Error') {
+        error('No Internet Or Server is not running');
+      } else {
+        error(e.response.data.error);
+      }
+    });
+}
+
+export const manageRequest = async (data) => {
+  await axios.patch('/admin/requests/pending', data);
+};
