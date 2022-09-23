58f0df750ba500775883764bb6a3389ba037b728
EidHachem <eidhachem1@gmail.com>
2022-09-23 09:41:19 +0300

    Add api request to get all reservations

--- src/Api/admins/requests/manageRequests.js
+++ src/Api/admins/requests/manageRequests.js
@@ -17,6 +17,23 @@ export default async function fetchRequests(error, stats) {
     });
 }
 
+export async function allRequests(error, stats) {
+  await axios
+    .get('/admin/reservations')
+    .then((res) => {
+      if (res.status === 200) {
+        stats(res.data);
+      }
+    })
+    .catch((e) => {
+      if (e.toJSON().message === 'Network Error') {
+        error('No Internet Or Server is not running');
+      } else {
+        error(e.response.data.error);
+      }
+    });
+}
+
 export const manageRequest = async (data) => {
   await axios.patch('/admin/requests/pending', data);
 };
