ffcea973773758c6b5c67a73f23f7ca812dd30fa
lyangenda <lyangendadavid@gmail.com>
2022-09-23 17:34:58 +0200

    Add logic function for USER  CRUD Admin page

--- src/Api/admins/users/UserApi.js
+++ src/Api/admins/users/UserApi.js
@@ -1,15 +1,44 @@
 import axios from '../../../config/axios';
 
-export default async function GetUsers(error, users) {
-  await axios.get('admin/users').then((res) => {
-    if (res.status === 200) {
-      users(res.data);
-    }
-  }).catch((e) => {
-    if (e.toJSON().message === 'Network Error') {
-      error('No Internet Or Server is not running');
-    } else {
-      error(e.response.data.error);
-    }
-  });
+export const fetchUsers = async () => {
+  let users = [];
+  await axios
+    .get('/users')
+    .then((response) => {
+      if (response.status === 200) {
+        users = response.data;
+      }
+    })
+    .catch((e) => console.log(e.response.data));
+  return users;
 }
+
+export const addUser = async (data) => axios
+  .post('/admin/users', data)
+  .then((response) => {
+    if (response.status === 200) {
+      return response;
+    }
+    return null;
+  })
+  .catch((e) => console.log(e.response.data));
+
+export const updateUser = async (data) => axios
+  .patch('/admin/users', data)
+  .then((response) => {
+    if (response.status === 200) {
+      return response;
+    }
+    return false;
+  })
+  .catch((e) => console.log(e.response.data));
+
+export const removeUser = async (id) => axios
+  .delete(`/admin/users/${id}`)
+  .then((response) => {
+    if (response.status === 200) {
+      return response.data;
+    }
+    return null;
+  })
+  .catch((e) => console.log(e.response.data));
