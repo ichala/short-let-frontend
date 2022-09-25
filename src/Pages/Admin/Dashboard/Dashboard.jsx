bafe2667f2ce315e96ec5693fb625a342306ffa2
Ali Jendoubi <alijandhd@gmail.com>
2022-09-26 00:12:53 +0100

    Update Route

--- src/Pages/Admin/Dashboard/Dashboard.jsx
+++ src/Pages/Admin/Dashboard/Dashboard.jsx
@@ -56,7 +56,7 @@ function Dashboard() {
     <>
       <div className="row m-1 ">
         <div className="col-md-3 mt-1 col-12 d-flex justify-content-center align-content-center">
-          <Link type="button" className="btn btn btn-outline-success" to="/admin/users"> Manage Users</Link>
+          <Link type="button" className="btn btn btn-outline-success" to="/admin/dashboard/users"> Manage Users</Link>
         </div>
         <div className="col-md-3 mt-1 col-12 d-flex justify-content-center align-content-center">
           <Link type="button" className="btn btn btn-outline-success" to="/admin/dashboard/requests"> Manage Reservations</Link>
@@ -65,7 +65,7 @@ function Dashboard() {
           <Link type="button" className="btn btn btn-outline-success" to="/admin/dashboard/pending-requests"> Pending Reservations</Link>
         </div>
         <div className="col-md-3 mt-1 col-12 d-flex justify-content-center align-content-center">
-          <Link type="button" className="btn btn btn-outline-success" to="/admin/dashboard/halls"> Manage Halls</Link>
+          <Link type="button" className="btn btn btn-outline-success" to="/admin/halls"> Manage Halls</Link>
         </div>
 
       </div>
