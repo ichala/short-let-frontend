08b88df0e0de3cb0da66240ff96862f041258df9
EidHachem <eidhachem1@gmail.com>
2022-09-26 20:13:03 +0300

    Edit Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,48 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import GetHalls from '../../Api/PublicApi/public_api';
-import HomeSlider from './Carousel/HomeSlider';
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 import Features from './Features/Features';
-import styles from './home.module.css';
+import './home.css';
 
 function Home() {
-  const [Halls, setHalls] = useState(null);
-  const [Error, setError] = useState(null);
-
-  useEffect(() => {
-    GetHalls(setError, setHalls);
-  }, []);
-
-  if (!Halls) {
-    return (
-      <>
-
-        {!Error ? (
-          <div className="d-flex justify-content-center">
-            <div className="spinner-grow text-lg text-center text-success" role="status" />
-          </div>
-        ) : (
-          <div className="alert alert-danger" role="alert">
-            {Error}
-          </div>
-        )}
-      </>
-    );
-  }
   return (
-    <>
-
-      <div className="row justify-content-center">
-        <img src="/images/logo.png" alt="logo" className={styles.logo} />
-      </div>
-
-      <div className="row">
-        <HomeSlider Halls={Halls} />
-      </div>
-
-      <div className="row mt-4">
+    <div className="overflow-hidden position-relative">
+      <div className="row mt-4 data">
+        <TypeAnimation
+          cursor={false}
+          sequence={[
+            'Find your dream house...',
+            1500,
+            'Find your dream room...',
+            1500,
+            'Find your dream apartment...',
+            1500,
+            'Find your dream chamber...',
+            1500,
+          ]}
+          speed={40}
+          wrapper="h1"
+          repeat={Infinity}
+          className="text-center mt-5 mb-5 text-black fw-bolder fs-2"
+        />
+        <img className="home-image" src="https://images.unsplash.com/photo-1530878902700-5ad4f9e4c318?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlsbGFnZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60" alt="Home" />
         <Features />
       </div>
-    </>
+    </div>
   );
 }
 
