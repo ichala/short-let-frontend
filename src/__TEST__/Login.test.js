a6e7ebba603e0e1b96834cd544e18ac7e7a78804
EidHachem <eidhachem1@gmail.com>
2022-09-26 14:14:29 +0300

    Add register and login components test

---
+++ src/__TEST__/Login.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../redux/store';
+import Login from '../Pages/Auth/Login';
+
+describe('Login', () => {
+  it('should render the login page', () => {
+    const app = render(
+      <Provider store={store}>
+        <Router>
+          <Login />
+        </Router>
+      </Provider>,
+    );
+    expect(app).toMatchSnapshot();
+  });
+});
---
+++ src/__TEST__/Register.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../redux/store';
+import Register from '../Pages/Auth/Register';
+
+describe('Register', () => {
+  it('should render the Register page', () => {
+    const app = render(
+      <Provider store={store}>
+        <Router>
+          <Register />
+        </Router>
+      </Provider>,
+    );
+    expect(app).toMatchSnapshot();
+  });
+});
