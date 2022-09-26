7beba29750a25698c46c63b710bf5875bab7ddc0
EidHachem <eidhachem1@gmail.com>
2022-09-26 21:20:36 +0300

    Update home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,9 +4,9 @@ import './home.css';
 
 function Home() {
   return (
-    <div className="overflow-hidden position-relative">
-      <div className="row mt-4 data">
-        <img className="home-image" src="https://images.unsplash.com/photo-1530878902700-5ad4f9e4c318?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlsbGFnZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60" alt="Home" />
+    <div className="overflow-hidden">
+      <div className="data mt-1">
+        <img className="home-image" src="https://images.unsplash.com/photo-1434434319959-1f886517e1fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80" alt="Home" />
         <Features />
       </div>
     </div>
