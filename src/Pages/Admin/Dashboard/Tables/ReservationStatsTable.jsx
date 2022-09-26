2e30316b5ff467de5ee25bb329ca84927ed12350
Aimal Amiri <meaimal@hotmail.com>
2022-09-26 18:37:06 +0430

    Transform RecentReservations table to DataTable

--- src/Pages/Admin/Dashboard/Tables/ReservationStatsTable.jsx
+++ src/Pages/Admin/Dashboard/Tables/ReservationStatsTable.jsx
@@ -1,7 +1,39 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
+import DataTable from 'react-data-table-component';
 import styles from '../dashboard.module.css';
 
 function ReservationStatsTable({ stats }) {
+  const columns = [
+    {
+      name: 'User',
+      selector: (row) => row.user_name,
+    },
+    {
+      name: 'Hall',
+      selector: (row) => row.hall_name,
+    },
+    {
+      name: 'Date',
+      selector: (row) => row.date,
+    },
+    {
+      name: 'Status',
+      button: true,
+      cell: (data) => (
+        <span className="d-none d-lg-block">
+          {data.status === 'Pending' ? (
+            <span className="bg-warning text-white fw-semibold rounded status">{data.status}</span>
+          ) : data.status === 'Confirmed' ? (
+            <span className="bg-success text-white fw-semibold rounded status">{data.status}</span>
+          ) : (
+            <span className="bg-danger text-white fw-semibold rounded status">{data.status}</span>
+          )}
+        </span>
+      ),
+    },
+  ];
+
   return (
     <div className="col-12 col-md-8">
       <div className="card h-100 text-center">
@@ -9,34 +41,9 @@ function ReservationStatsTable({ stats }) {
           <h5>Recent Reservations</h5>
         </div>
         <div className="card-body">
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">User</th>
-                <th scope="col">Hall</th>
-                <th scope="col">Date</th>
-                <th scope="col">Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {stats.map((user, i) => (
-                <tr key={i}>
-                  <td>{user.user_name}</td>
-                  <td>{user.hall_name}</td>
-                  <td>{user.date}</td>
-                  <td>
-                    {user.status === 'Confirmed' && <span className="badge rounded-pill bg-success">Confirmed</span>}
-                    {user.status === 'Pending' && <span className="badge rounded-pill bg-dark">Pending</span>}
-                    {user.status === 'Refused' && <span className="badge rounded-pill bg-danger">Refused</span>}
-                  </td>
-                </tr>
-              ))}
-
-            </tbody>
-          </table>
+          <DataTable columns={columns} data={stats} highlightOnHover pagination responsive />
         </div>
       </div>
-
     </div>
   );
 }
