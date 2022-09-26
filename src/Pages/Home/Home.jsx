3c268445693f5e5d4808785e38abb228d947f0f2
EidHachem <eidhachem1@gmail.com>
2022-09-26 20:34:53 +0300

    Remove animation library

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { TypeAnimation } from 'react-type-animation';
 import Features from './Features/Features';
 import './home.css';
 
@@ -7,23 +6,6 @@ function Home() {
   return (
     <div className="overflow-hidden position-relative">
       <div className="row mt-4 data">
-        <TypeAnimation
-          cursor={false}
-          sequence={[
-            'Find your dream house...',
-            1500,
-            'Find your dream room...',
-            1500,
-            'Find your dream apartment...',
-            1500,
-            'Find your dream chamber...',
-            1500,
-          ]}
-          speed={40}
-          wrapper="h1"
-          repeat={Infinity}
-          className="text-center mt-5 mb-5 text-black fw-bolder fs-2"
-        />
         <img className="home-image" src="https://images.unsplash.com/photo-1530878902700-5ad4f9e4c318?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlsbGFnZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60" alt="Home" />
         <Features />
       </div>
