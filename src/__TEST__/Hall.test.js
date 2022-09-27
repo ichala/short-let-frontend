2cdd529d864cd3a514812499488cd5195b16b278
Ali Jendoubi <alijandhd@gmail.com>
2022-09-27 10:30:14 +0100

    Update Test

--- src/__TEST__/Hall.test.js
+++ src/__TEST__/Hall.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../redux/store';
-import Hall from '../Pages/Hall/index';
+import Hall from '../Pages/Admin/Hall/index';
 
 describe('Login', () => {
   it('should render the login page', () => {
